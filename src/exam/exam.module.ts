5157002f8200fbea5870f01a7375e5dc78b30099
Damoz <panchitodmun@gmail.com>
2024-03-13 12:40:05 -0500

    :sparkles: Exam module

--- src/exam/exam.module.ts
+++ src/exam/exam.module.ts
@@ -3,10 +3,11 @@ import { ExamService } from './exam.service';
 import { ExamController } from './exam.controller';
 import { SqlDatabaseModule } from 'src/shared';
 import { Exam } from './entities/exam.entity';
+import { ExamRepository } from './exam.repository';
 
 @Module({
   imports: [SqlDatabaseModule.forFeature([Exam])],
   controllers: [ExamController],
-  providers: [ExamService]
+  providers: [ExamService, ExamRepository]
 })
 export class ExamModule { }
