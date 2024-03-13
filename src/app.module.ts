5121372ece97195a37a6a154a1d71a3d91a6618f
Damoz <panchitodmun@gmail.com>
2024-03-13 09:29:58 -0500

    :sparkles: Custom Logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SqlDatabaseModule } from './shared';
 import { WebTokenModule } from './shared/web-token/web-token.module';
+import { LoggerModule } from './shared/logger/logger.module';
 
 @Module({
-  imports: [SqlDatabaseModule, WebTokenModule],
+  imports: [SqlDatabaseModule, WebTokenModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LoggerModule } from './shared/logger/logger.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(LoggerModule))
   await app.listen(3000);
 }
 bootstrap();
