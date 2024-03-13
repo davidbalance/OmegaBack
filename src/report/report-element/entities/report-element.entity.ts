c4b8533d763eac7252f0f8daaf16492433dc5fd3
Damoz <panchitodmun@gmail.com>
2024-03-13 13:01:18 -0500

    :sparkles: Report entities

--- src/report/report-element/entities/report-element.entity.ts
+++ src/report/report-element/entities/report-element.entity.ts
@@ -1,1 +1,17 @@
-export class ReportElement {}
+import { ReportElementType } from "src/report/common/enums";
+import { AbstractEntity } from "src/shared";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'REPORT_ELEMENTS' })
+export class ReportElement extends AbstractEntity<number> {
+    @PrimaryGeneratedColumn('increment', { name: 'REPORT_ELEMENT_ID' })
+    public id: number;
+    @Column({ name: 'REPORT_ELEMENT_NAME', type: 'varchar', length: 64 })
+    public name: string;
+    @Column({ name: 'REPORT_ELEMENT_TYPE', type: 'enum', enum: ReportElementType, nullable: false })
+    public type: ReportElementType;
+    @Column({ name: 'REPORT_ELEMENT_MANDATORY', type: 'boolean', default: false, nullable: false })
+    public mandatory: boolean;
+    @Column({ name: 'REPORT_ELEMENT_STATUS', type: 'boolean', default: true, nullable: false })
+    public status: boolean;
+}
--- src/report/report-element/report-element.module.ts
+++ src/report/report-element/report-element.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportElementService } from './report-element.service';
 import { ReportElementController } from './report-element.controller';
+import { SqlDatabaseModule } from 'src/shared';
+import { ReportElement } from './entities/report-element.entity';
 
 @Module({
+  imports: [SqlDatabaseModule.forFeature([ReportElement])],
   controllers: [ReportElementController],
   providers: [ReportElementService]
 })
-export class ReportElementModule {}
+export class ReportElementModule { }
