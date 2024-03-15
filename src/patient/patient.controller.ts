2a82712da1b20b6a2116745d0e22d6f2833df445
Damoz <panchitodmun@gmail.com>
2024-03-15 15:21:59 -0500

    :sparkles: Patient controller

--- src/patient/patient.controller.ts
+++ src/patient/patient.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PatientService } from './patient.service';
+import { Patient } from './entities/patient.entity';
+import { CreatePatientRequestDTO, UpdatePatientRequestDTO } from 'src/shared';
 
-@Controller('patient')
+@Controller('patients')
 export class PatientController {
   constructor(private readonly patientService: PatientService) { }
+
+  @Post()
+  async create(
+    @Body() body: CreatePatientRequestDTO
+  ): Promise<void> {
+    await this.patientService.create(body);
+  }
+
+  @Get()
+  async readAll(): Promise<Patient[]> {
+    return await this.patientService.readAll();
+  }
+
+  @Get('patient/:id')
+  async readOneByID(
+    @Param('id') id: number
+  ): Promise<Patient> {
+    return await this.patientService.readOneByID(id);
+  }
+
+  @Patch('patient/:id')
+  async update(
+    @Param('id') id: number,
+    @Body() body: UpdatePatientRequestDTO
+  ): Promise<void> {
+    await this.patientService.update(id, body);
+  }
+
 }
