9607e0aaa882bfd2f42dd66c99011b0d28330081
Damoz <panchitodmun@gmail.com>
2024-03-15 14:41:10 -0500

    :sparkles: LocalStorage service

--- src/shared/storage-saver/local-storage-saver/local-storage-saver.service.ts
+++ src/shared/storage-saver/local-storage-saver/local-storage-saver.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { StorageSaver } from '../storage-saver.service';
+import { createReadStream, createWriteStream } from 'fs';
+import { extname } from 'path';
+import { v4 } from 'uuid';
 
 @Injectable()
 export class LocalStorageSaverService implements StorageSaver {
-    saveFile(file: Express.Multer.File): string | Promise<string> {
-        throw new Error('Method not implemented.');
+    saveFile(file: Express.Multer.File): string | Promise<string>;
+    saveFile(file: Express.Multer.File, dir: string): string | Promise<string>;
+    saveFile(file: Express.Multer.File, dir: string = "tmp"): string | Promise<string> {
+        const extension = extname(file.originalname);
+        const filename = v4();
+        const fileStream = createReadStream(`${file.path}/${filename}${extension}`);
+        const writeStream = createWriteStream(dir);
+        fileStream.pipe(writeStream);
+
+        return new Promise<string>((resolve, reject) => {
+            writeStream.on('finish', () => resolve(`${filename}${extension}`));
+            writeStream.on('error', (error) => reject(error));
+        });
     }
 }
--- src/shared/storage-saver/storage-saver.service.ts
+++ src/shared/storage-saver/storage-saver.service.ts
@@ -1,3 +1,13 @@
 export abstract class StorageSaver {
+    /**
+     * Save file in a tmp directory in the localmachine
+     * @param file 
+     */
     abstract saveFile(file: Express.Multer.File): string | Promise<string>
+    /**
+     * Saves file in the given directory in the localmachine
+     * @param file 
+     * @param dir 
+     */
+    abstract saveFile(file: Express.Multer.File, dir: string): string | Promise<string>
 }
