07de82e7059611e8a6feafb9e86e17732b254c3c
Damoz <panchitodmun@gmail.com>
2024-05-19 15:33:29 -0500

    Modify deploy script

--- .github/scripts/connect-to-server.sh
+++ .github/scripts/connect-to-server.sh
@@ -5,7 +5,7 @@ SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY}
 SERVER_HOST=${SERVER_HOST}
 
 # Connect to the server using SSH with the agent (no key management needed)
-ssh -o StrictHostKeyChecking=no  -i /tmp/ssh_key $SERVER_HOST
+ssh -o StrictHostKeyChecking=no  -i $SSH_PRIVATE_KEY $SERVER_HOST
 
 # You can add additional commands here to execute on the server after connecting
 # For example, you might transfer files, run scripts on the server, etc.
