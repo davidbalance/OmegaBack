3d51b7ec6de520055a6147d7f67fd354280910d6
Damoz <panchitodmun@gmail.com>
2024-06-24 15:07:59 -0500

    :sparkles: Medical report controller add guards

--- src/medical/medical-report/medical-report.controller.ts
+++ src/medical/medical-report/medical-report.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Inject, Param, Post } from "@nestjs/common";
+import { Controller, Get, Inject, Param, Post, UseGuards } from "@nestjs/common";
 import { MedicalReportService } from "./medical-report.service";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "@/shared/guards/authentication-guard";
 
 @ApiTags('Medical/Report')
 @ApiBearerAuth()
@@ -10,12 +11,14 @@ export class MedicalReportController {
         @Inject(MedicalReportService) private readonly service: MedicalReportService
     ) { }
 
+    @UseGuards(JwtAuthGuard)
     @Get('recreate/pdf')
     async recreateAllReports(): Promise<any> {
         await this.service.recreateReport();
         return {};
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('recreate/pdf/:dni')
     async recreateReportsByPatientDni(
         @Param('dni') dni: string
