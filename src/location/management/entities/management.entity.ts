b764ee681c30a4fe37404440c34410d767f71d16
Damoz <panchitodmun@gmail.com>
2024-07-10 12:53:22 -0500

    :sparkles: Location Management Entity intialize

--- src/location/management/entities/management.entity.ts
+++ src/location/management/entities/management.entity.ts
@@ -1,1 +1,14 @@
-export class Management {}
+import { AbstractEntity } from "@/shared";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('tbl_lo_management')
+export class Management extends AbstractEntity<number> {
+    @PrimaryGeneratedColumn('increment', { name: 'management_id' })
+    public id: number;
+
+    @Column({ name: 'management_name', type: 'varchar', length: 128, nullable: false })
+    public name: string;
+
+    @Column({ name: 'management_status', type: 'boolean', default: true, nullable: false })
+    public status: boolean;
+}
