500cdf0a8dea881e3a69dcecd9b88a022d88f477
Damoz <panchitodmun@gmail.com>
2024-07-10 12:47:59 -0500

    :sparkles: Location management module

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -3,13 +3,15 @@ import { CityModule } from './city/city.module';
 import { CompanyModule } from './company/company.module';
 import { CorporativeGroupModule } from './corporative-group/corporative-group.module';
 import { BranchModule } from './branch/branch.module';
+import { ManagementModule } from './management/management.module';
 
 @Module({
   imports: [
     CityModule,
     CompanyModule,
     CorporativeGroupModule,
-    BranchModule
+    BranchModule,
+    ManagementModule
   ]
 })
 export class LocationModule { }
