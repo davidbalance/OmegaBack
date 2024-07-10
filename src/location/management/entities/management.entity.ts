99acc85df217a10d679ded93c3d8924b7f49e8af
Damoz <panchitodmun@gmail.com>
2024-07-10 12:58:17 -0500

    :sparkles: Location Area entity with Management Relation

--- src/location/management/entities/management.entity.ts
+++ src/location/management/entities/management.entity.ts
@@ -1,7 +1,8 @@
+import { Area } from "@/location/area/entities/area.entity";
 import { AbstractEntity } from "@/shared";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity('tbl_lo_management')
+@Entity('tbl_lo_managements')
 export class Management extends AbstractEntity<number> {
     @PrimaryGeneratedColumn('increment', { name: 'management_id' })
     public id: number;
@@ -11,4 +12,7 @@ export class Management extends AbstractEntity<number> {
 
     @Column({ name: 'management_status', type: 'boolean', default: true, nullable: false })
     public status: boolean;
+
+    @OneToMany(() => Area, area => area.management, { eager: true, nullable: true })
+    public areas: Area[];
 }
