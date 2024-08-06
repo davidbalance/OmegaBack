e00d2f54ffda2eb0bac6c7e23d6784c4785aceae
Damoz <panchitodmun@gmail.com>
2024-08-06 15:55:31 -0500

    :white_check_mark: Add branch external listener test

---
+++ src/location/branch/listeners/branch-external.listener.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed } from "@automock/jest";
+import { BranchExternalConnectionService } from "../services/branch-external-connection.service";
+import { BranchExternalListener } from "./branch-external.listener";
+import { BranchExternalCreateEvent } from "@/shared/events/branch.event";
+import { PostBranchExternalRequestDto } from "../dtos/request/post.branch-external.request.dto";
+
+describe('BranchExternalListener', () => {
+    let service: BranchExternalListener;
+    let externalConnectionService: jest.Mocked<BranchExternalConnectionService>;
+
+    beforeEach(async () => {
+        const { unit, unitRef } = TestBed.create(BranchExternalListener).compile();
+
+        service = unit;
+        externalConnectionService = unitRef.get(BranchExternalConnectionService);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('onExternalCreate', () => {
+        const key = 'key';
+        const source = 'source';
+        const mockData: PostBranchExternalRequestDto = {
+            company: undefined,
+            name: "",
+            city: ""
+        };
+
+        it('should call the external service to create a new branch', async () => {
+            // Arrange
+            externalConnectionService.create.mockResolvedValue(undefined);
+
+            // Act
+            await service.onExternalCreate({ key, source, data: mockData });
+
+            // Assert
+            expect(externalConnectionService.create).toHaveBeenCalledWith({ key, source }, mockData);
+        });
+    });
+});
