4e75f4321f158056aa61f800452816c16d793c39
Damoz <panchitodmun@gmail.com>
2024-08-08 15:48:05 -0500

    :white_check_mark: Add corporative group management test

--- src/location/corporative-group/services/corporative-group-management.service.ts
+++ src/location/corporative-group/services/corporative-group-management.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CorporativeGroupRepository } from '../repositories/corporative-group.repository';
 import { CorporativeGroup } from '../entities/corporative-group.entity';
+import { PostCorporativeGroupRequestDto } from '../dtos/request/post.corporative-group.dto';
 
 @Injectable()
 export class CorporativeGroupManagementService {
@@ -9,19 +10,27 @@ export class CorporativeGroupManagementService {
     @Inject(CorporativeGroupRepository) private readonly repository: CorporativeGroupRepository
   ) { }
 
+  async create(data: PostCorporativeGroupRequestDto): Promise<CorporativeGroup> {
+    return this.repository.create(data);
+  }
+
   async find(): Promise<CorporativeGroup[]> {
     const groups = await this.repository.find({
-      where: {
-        status: true
-      },
-      relations: {
-        companies: {
-          branches: {
-            city: true
-          }
-        }
-      }
+      where: { status: true },
+      relations: { companies: { branches: { city: true } } }
     });
     return groups;
+  }
+
+  async findOne(id: number): Promise<CorporativeGroup> {
+    return await this.repository.findOne({ where: { id }, relations: { companies: { branches: true } } });
+  }
+
+  async updateOne(id: number, body: PostCorporativeGroupRequestDto): Promise<CorporativeGroup> {
+    return await this.repository.findOneAndUpdate({ id }, body);
+  }
+
+  async deleteOne(id: number): Promise<void> {
+    await this.repository.findOneAndDelete({ id });
   }
 }
--- src/location/corporative-group/services/test/corporative-group-management.service.spec.ts
+++ src/location/corporative-group/services/test/corporative-group-management.service.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from "@automock/jest";
 import { CorporativeGroupRepository } from "../../repositories/corporative-group.repository";
 import { CorporativeGroupManagementService } from "../corporative-group-management.service";
-import { mockCorporativeGroups } from "./stub/corporative-group.stub";
+import { mockCorporativeGroup, mockCorporativeGroups } from "./stub/corporative-group.stub";
+import { PostCorporativeGroupRequestDto } from "../../dtos/request/post.corporative-group.dto";
 
 describe('CorporativeGroupManagementService', () => {
   let service: CorporativeGroupManagementService;
@@ -18,14 +19,94 @@ describe('CorporativeGroupManagementService', () => {
     jest.clearAllMocks();
   });
 
+  describe('create', () => {
+    const mockedGroup = mockCorporativeGroup();
+    const body: PostCorporativeGroupRequestDto = {
+      name: "Corporative name"
+    }
+
+    it('should create a corporative group', async () => {
+      // Arrange
+      repository.create.mockResolvedValueOnce(mockedGroup);
+
+      // Act
+      const result = await service.create(body);
+
+      // Assert
+      expect(result).toEqual(mockedGroup);
+      expect(repository.create).toHaveBeenCalledWith(body);
+    });
+  });
+
   describe('find', () => {
     const mockedGroups = mockCorporativeGroups();
 
-    it('should return corporative groups', async () => {
+    it('should return all corporative groups', async () => {
+      // Arrange
       repository.find.mockResolvedValueOnce(mockedGroups);
+
+      // Act
       const result = await service.find();
 
+      // Assert
       expect(result).toEqual(mockedGroups);
+      expect(repository.find).toHaveBeenCalledWith({
+        where: { status: true },
+        relations: { companies: { branches: { city: true } } }
+      });
     });
   });
+
+  describe('findOne', () => {
+    const id: number = 1;
+    const mockedGroup = mockCorporativeGroup();
+
+    it('should return a corporative group by id', async () => {
+      // Arrange
+      repository.findOne.mockResolvedValueOnce(mockedGroup);
+
+      // Act
+      const result = await service.findOne(id);
+
+      // Assert
+      expect(result).toEqual(mockedGroup);
+      expect(repository.findOne).toHaveBeenCalledWith({ where: { id }, relations: { companies: { branches: true } } });
+    });
+  });
+
+  describe('updateOne', () => {
+    const id: number = 1;
+    const mockedGroup = mockCorporativeGroup();
+    const body: PostCorporativeGroupRequestDto = {
+      name: "Updated corporative group name"
+    }
+
+    it('should update a corporative group', async () => {
+      // Arrange
+      repository.findOneAndUpdate.mockResolvedValueOnce(mockedGroup);
+
+      // Act
+      const result = await service.updateOne(id, body);
+
+      // Assert
+      expect(result).toEqual(mockedGroup);
+      expect(repository.findOneAndUpdate).toHaveBeenCalledWith({ id }, body);
+    });
+  });
+
+  describe('deleteOne', () => {
+    const id: number = 1;
+
+    it('should delete a corporative group', async () => {
+      // Arrange
+      repository.findOneAndDelete.mockResolvedValueOnce(undefined);
+
+      // Act
+      await service.deleteOne(id);
+
+      // Assert
+      expect(repository.findOneAndDelete).toHaveBeenCalledWith({ id });
+    });
+  });
+
 });
