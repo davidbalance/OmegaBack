a9cd53bfb130e4e9e5d679a44cfd80844041fe17
Damoz <panchitodmun@gmail.com>
2024-08-09 16:38:03 -0500

    :white_check_mark: Add medical order management controller test

---
+++ src/medical/medical-order/controllers/medical-order-status.controller.ts
@@ -0,0 +1,33 @@
+import { JwtAuthGuard } from "@/shared/guards/authentication-guard";
+import { Controller, Param, UseGuards, Patch, Inject } from "@nestjs/common";
+import { ApiTags, ApiBearerAuth } from "@nestjs/swagger";
+import { plainToInstance } from "class-transformer";
+import { OrderStatus } from "../enums";
+import { MedicalOrderManagementService } from "../services/medical-order-management.service";
+import { GetMedicalOrderResponseDto } from "../dtos/response/get.medical-order.response.dto";
+
+@ApiTags('Medical/Order')
+@ApiBearerAuth()
+@Controller('medical/orders/:id/status')
+@UseGuards(JwtAuthGuard)
+export class MedicalOrderManagementController {
+  constructor(
+    @Inject(MedicalOrderManagementService) private readonly service: MedicalOrderManagementService
+  ) { }
+
+  @Patch('validate')
+  async findOneAndValidateStatus(
+    @Param('id') id: number
+  ): Promise<GetMedicalOrderResponseDto> {
+    const order = await this.service.updateOne(id, { orderStatus: OrderStatus.VALIDATED });
+    return plainToInstance(GetMedicalOrderResponseDto, order);
+  }
+
+  @Patch('created')
+  async findOneAndCratedStatus(
+    @Param('id') id: number
+  ): Promise<GetMedicalOrderResponseDto> {
+    const order = await this.service.updateOne(id, { orderStatus: OrderStatus.CREATED });
+    return plainToInstance(GetMedicalOrderResponseDto, order);
+  }
+}
