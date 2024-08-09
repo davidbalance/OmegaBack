a0c2789e743cf9c629877d5d5f2397e0a35199be
Damoz <panchitodmun@gmail.com>
2024-08-09 12:46:13 -0500

    :white_check_mark: Add mail client job position controller test

---
+++ src/medical/medical-client/services/test/stub/medical-client.stub.ts
@@ -0,0 +1,22 @@
+import { MedicalClient } from "@/medical/medical-client/entities/medical-client.entity";
+
+const stubMedicalClient = (id: number): MedicalClient => ({
+    id: id,
+    dni: "1234567890",
+    name: "Mocked name",
+    lastname: "Mocked lastname",
+    birthday: new Date('2000/01/01'),
+    gender: "male",
+    jobPositionName: "Test position",
+    managementId: 1,
+    managementName: "Test management",
+    areaId: 1,
+    areaName: "Test area",
+    medicalOrders: [],
+    email: [],
+    createAt: new Date(),
+    updateAt: new Date()
+});
+
+export const mockMedicalClient = () => stubMedicalClient(1);
+export const mockMedicalClients = () => [1, 2, 3, 4, 5].map(stubMedicalClient);
