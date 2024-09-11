4ba95457ebe14d2cb01425a60bf3a7ecad802dfe
Damoz <panchitodmun@gmail.com>
2024-09-11 18:52:44 -0500

    Remove send-attribute

--- src/shared/send-attribute/send-attribute.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, Index } from "typeorm";
-import { AbstractEntity } from "../sql-database/abstract.entity";
-
-@Index('idx_send_attribute_value', ['value'], { unique: false })
-export abstract class AbstractSendAttributeEntity extends AbstractEntity<number> {
-    @Column({ name: 'send_attribute_value', type: 'varchar', length: 128, nullable: false })
-    public value: string;
-}
