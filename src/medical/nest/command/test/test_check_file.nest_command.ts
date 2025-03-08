cd81f4dba90a359e1ba6c384e8d66fdfa8003ff3
Damoz <panchitodmun@gmail.com>
2025-03-08 10:01:25 -0500

    Patch: File checker

--- src/medical/nest/command/test/test_check_file.nest_command.ts
+++ src/medical/nest/command/test/test_check_file.nest_command.ts
@@ -3,16 +3,19 @@ import { TestCheckFileCommandToken } from "../../inject/command.inject";
 import { TestCheckFileCommand } from "@omega/medical/application/commands/test/test-check-file.command";
 import { InjectModelRepository } from "../../inject/model-repository.inject";
 import { TestFileResultRepository } from "@omega/medical/application/repository/model.repositories";
-import { InjectQuery } from "../../inject/query.inject";
-import { ResultGetFileQuery } from "@omega/medical/application/queries/test/result-get-file.query";
+import { InjectAggregateRepository } from "../../inject/aggregate-repository.inject";
+import { TestRepository } from "@omega/medical/application/repository/aggregate.repositories";
+import { InjectFile } from "@shared/shared/nest/inject";
+import { FileOperation } from "@shared/shared/providers";
 
 @Injectable()
 class TestCheckFileNestCommand extends TestCheckFileCommand {
     constructor(
         @InjectModelRepository("TestFileResult") model: TestFileResultRepository,
-        @InjectQuery('ResultGetFile') file: ResultGetFileQuery
+        @InjectAggregateRepository('Test') repository: TestRepository,
+        @InjectFile() file: FileOperation
     ) {
-        super(model, file);
+        super(model, repository, file);
     }
 }
 
