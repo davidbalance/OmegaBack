ff16872ca172ffb1e4555dc6c777171ab3298b71
Damoz <panchitodmun@gmail.com>
2025-04-28 12:26:36 -0500

    Massive load improve

--- src/medical/controller/mapper/order-massive-load.spreadsheet-mapper.ts
+++ src/medical/controller/mapper/order-massive-load.spreadsheet-mapper.ts
@@ -5,8 +5,9 @@ import { BadRequestException } from "@nestjs/common";
 export class OrderMassiveLoadSpreadSheetMapper {
 
     public static toDTO(value: unknown[], types: string[], subtypes: string[], exams: string[]): OrderMassiveLoadRequestDto {
-        if (value.length < 12) throw new BadRequestException('Not enough values.');
+        if (value.length < 9) throw new BadRequestException('Not enough values.');
         if (types.length !== subtypes.length && types.length !== exams.length) throw new BadRequestException('Not valid exams.');
+
 
         const newTests = value.slice(9).map((e, i) => !!e && typeof e === 'string' && e.trim() !== '' ? ({
             examType: types[i],
@@ -14,13 +15,17 @@ export class OrderMassiveLoadSpreadSheetMapper {
             examName: exams[i],
         }) : null).filter(e => !!e);
 
+        if (!newTests.length) {
+            throw new BadRequestException(`At least you need one test. Patient=${value[0]}`);
+        }
+
         return plainToInstance(OrderMassiveLoadRequestDto, {
             patientDni: this.validateDni(value[0]),
             corporativeName: value[1],
             companyRuc: this.validateRuc(value[2]),
             companyName: value[3],
             branchName: value[4],
-            doctorDni: this.validateDni(value[5]),
+            doctorDni: value[5] ? this.validateDni(value[5]) : undefined,
             doctorFullname: value[6],
             process: value[7],
             year: value[8],
@@ -32,7 +37,7 @@ export class OrderMassiveLoadSpreadSheetMapper {
         if (typeof value === 'number') {
             return value.toString().padStart(10, '0');
         } else if (typeof value === 'string') {
-            return value;
+            return value.padStart(10, '0');
         }
         throw new BadRequestException('Invalid dni');
     }
