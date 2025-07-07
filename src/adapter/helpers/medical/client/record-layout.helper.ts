af7b2e88ade2255d59f39be2468e173ea350c84a
Damoz <panchitodmun@gmail.com>
2025-07-06 20:56:12 -0500

    Add time zone

--- src/adapter/helpers/medical/client/record-layout.helper.ts
+++ src/adapter/helpers/medical/client/record-layout.helper.ts
@@ -2,6 +2,7 @@ import { formatDate } from "date-fns";
 import { createRecordLayout } from "./generic-record-helper";
 import { ClientRecordLayoutFunc, GenericRecord } from "@omega/medical/application/commands/client/client-add-record.command";
 import { craftCell, craftRow, craftSpacing, craftTable } from "./table.helper";
+import { TZDate } from "@date-fns/tz";
 
 const recordTitle = (value: Pick<GenericRecord, 'type'>): string => {
     switch (value.type) {
@@ -23,8 +24,9 @@ const recordMessage = (value: Pick<GenericRecord, 'type'>): string => {
     }
 }
 
+TZDate.tz("America/Guayaquil");
 const footer = (value: Pick<GenericRecord, 'authorDni' | 'authorFullname' | 'type'>) => {
-    const date = new Date();
+    const date = new TZDate();
     const formatedDate = formatDate(date, 'yyyy/MM/dd');
     const formatedHour = formatDate(date, 'HH:mm');
 
